8414999119bd598a99b0a89fa1b15f37fada15dd
aaapliii23 <abdulrapli05@gmail.com>
2025-10-17 08:54:29 +0700

    Delete berita directory

--- berita/bahasa_indonesia.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$lang_judul = "Beranda";
-	$lang_selamat_datang = "Hai, selamat datang di website kami";
-	$lang_menu_home = "Beranda";
-	$lang_menu_profile = "Profil";
-	$lang_menu_contact = "Kontak Kami";
-	$lang_menu_add = "Tambah Berita";
-?>
